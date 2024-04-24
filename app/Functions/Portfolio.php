fb273d5c87912ac528c6f298ea453a7165336517
loehur <loehur@gmail.com>
2024-04-24 10:24:12 +0700

    gas checkin2s

--- app/Functions/Portfolio.php
+++ app/Functions/Portfolio.php
@@ -17,17 +17,19 @@ class Portfolio extends Controller
 
    function daily_checkin()
    {
-      $log = $_SESSION['log'];
-      if (!isset($_SESSION['portfolio']['user_id'])) {
-         $_SESSION['portfolio'] = $this->func("Portfolio")->portfolio();
-      }
-      $data = $_SESSION['portfolio'];
+      if (isset($_SESSION['log'])) {
+         if (!isset($_SESSION['portfolio']['user_id'])) {
+            $_SESSION['portfolio'] = $this->func("Portfolio")->portfolio();
+         }
+         $data = $_SESSION['portfolio'];
 
-      $c = [];
-      if (isset($data['port_id'])) {
-         $c = $this->db(0)->get_where_row("daily_checkin", "ref = '" . $data['port_id'] . "'");
+         $c = [];
+         if (isset($data['port_id'])) {
+            $c = $this->db(0)->get_where_row("daily_checkin", "ref = '" . $data['port_id'] . "'");
+         }
+         return $c;
+      } else {
+         return [];
       }
-
-      return $c;
    }
 }
