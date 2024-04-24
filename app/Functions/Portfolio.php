dd35de98012dcce121cf85d6970766ea19217b76
loehur <loehur@gmail.com>
2024-04-24 10:23:35 +0700

    gas checkin2

--- app/Functions/Portfolio.php
+++ app/Functions/Portfolio.php
@@ -4,11 +4,15 @@ class Portfolio extends Controller
 {
    function portfolio()
    {
-      $log = $_SESSION['log'];
-      $active = $this->db(0)->get_where_row("portfolio", "user_id = '" . $log['user_id'] . "' AND port_status = 0");
-      $saldo_portfolio = $this->db(0)->get_cols_where("balance", "SUM(amount) as amount", "user_id = '" . $log['user_id'] . "' AND tr_status <> 2", 0);
-      $active['saldo'] = $saldo_portfolio['amount'];
-      return $active;
+      if (isset($_SESSION['log'])) {
+         $log = $_SESSION['log'];
+         $active = $this->db(0)->get_where_row("portfolio", "user_id = '" . $log['user_id'] . "' AND port_status = 0");
+         $saldo_portfolio = $this->db(0)->get_cols_where("balance", "SUM(amount) as amount", "user_id = '" . $log['user_id'] . "' AND tr_status <> 2", 0);
+         $active['saldo'] = $saldo_portfolio['amount'];
+         return $active;
+      } else {
+         return [];
+      }
    }
 
    function daily_checkin()
