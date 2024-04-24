783d5f563aa832fa337fe6734d37797457aa3768
loehur <loehur@gmail.com>
2024-04-24 12:11:23 +0700

    upgrades2e

--- app/Functions/Portfolio.php
+++ app/Functions/Portfolio.php
@@ -7,11 +7,15 @@ class Portfolio extends Controller
       if (isset($_SESSION['log'])) {
          $log = $_SESSION['log'];
          $active = $this->db(0)->get_where_row("portfolio", "user_id = '" . $log['user_id'] . "' AND port_status = 0");
-         $saldo_portfolio = $this->db(0)->get_cols_where("balance", "SUM(amount) as amount", "user_id = '" . $log['user_id'] . "' AND balance_type = 10 AND flow = 2 AND ref = '" . $active['port_id'] . "' AND tr_status <> 2", 0);
-         $total_portfolio = $this->db(0)->get_cols_where("balance", "SUM(amount) as amount", "user_id = '" . $log['user_id'] . "' AND (balance_type BETWEEN 20 AND 23) AND ref = '" . $active['port_id'] . "' AND tr_status <> 2", 0);
-         $active['saldo'] = $saldo_portfolio['amount'];
-         $active['fee'] = $total_portfolio['amount'];
-         return $active;
+         if (isset($active['port_id'])) {
+            $saldo_portfolio = $this->db(0)->get_cols_where("balance", "SUM(amount) as amount", "user_id = '" . $log['user_id'] . "' AND balance_type = 10 AND flow = 2 AND ref = '" . $active['port_id'] . "' AND tr_status <> 2", 0);
+            $total_portfolio = $this->db(0)->get_cols_where("balance", "SUM(amount) as amount", "user_id = '" . $log['user_id'] . "' AND (balance_type BETWEEN 20 AND 23) AND ref = '" . $active['port_id'] . "' AND tr_status <> 2", 0);
+            $active['saldo'] = $saldo_portfolio['amount'];
+            $active['fee'] = $total_portfolio['amount'];
+            return $active;
+         } else {
+            return [];
+         }
       } else {
          return [];
       }
