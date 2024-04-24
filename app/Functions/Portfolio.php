6a75c95b0e447533671e272772691b4b0a4619ed
loehur <loehur@gmail.com>
2024-04-24 10:38:57 +0700

    fix bug21

--- app/Functions/Portfolio.php
+++ app/Functions/Portfolio.php
@@ -7,7 +7,7 @@ class Portfolio extends Controller
       if (isset($_SESSION['log'])) {
          $log = $_SESSION['log'];
          $active = $this->db(0)->get_where_row("portfolio", "user_id = '" . $log['user_id'] . "' AND port_status = 0");
-         $saldo_portfolio = $this->db(0)->get_cols_where("balance", "SUM(amount) as amount", "user_id = '" . $log['user_id'] . "' AND tr_status <> 2", 0);
+         $saldo_portfolio = $this->db(0)->get_cols_where("balance", "SUM(amount) as amount", "user_id = '" . $log['user_id'] . "' AND ref = '" . $active['port_id'] . "' AND tr_status <> 2", 0);
          $active['saldo'] = $saldo_portfolio['amount'];
          return $active;
       } else {
