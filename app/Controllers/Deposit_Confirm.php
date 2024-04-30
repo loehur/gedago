6bc57ef74be39bbe1370971ade599c45ebdecb84
loehur <loehur@gmail.com>
2024-04-30 21:14:14 +0700

    benerin confirm deposde

--- app/Controllers/Deposit_Confirm.php
+++ app/Controllers/Deposit_Confirm.php
@@ -24,12 +24,10 @@ class Deposit_Confirm extends Controller
    public function content()
    {
       $data['dep'] = $this->db(0)->get_where_row("balance", "user_id = '" . $_SESSION['log']['user_id'] . "' AND user_confirm = 0 AND balance_type = 1 AND flow = 1 AND tr_status = 0 ORDER BY insertTime DESC");
-
-      if (count($data['dep']) == 0) {
-         header("Location: " . PC::BASE_URL . "Deposit");
-         exit();
-      } else {
+      if (isset($data['dep']['user_id'])) {
          $this->view(__CLASS__, __CLASS__ . "/content", $data);
+      } else {
+         echo "Data already updated!";
       }
    }
 
