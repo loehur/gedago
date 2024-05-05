fc0c17f4df2c0f3db2e53def252d10d9a7db5a42
loehur <loehur@gmail.com>
2024-05-05 09:19:57 +0700

    benerin timezoneadf

--- app/Controllers/Cron.php
+++ app/Controllers/Cron.php
@@ -71,5 +71,7 @@ class Cron extends Controller
    {
       date_default_timezone_set("Asia/Jakarta");
       echo date("Y-m-d H:i:s");
+      echo "<br>";
+      print_r($this->db(0)->query("SET time_zone = 'Asia/Jakarta'"));
    }
 }
