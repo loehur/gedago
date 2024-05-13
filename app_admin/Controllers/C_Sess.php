0fc0b3acb32395f1aa29bd311b6edfcd4f9b2676
loehur <loehur@gmail.com>
2024-05-13 16:31:41 +0700

    benerin menu222223

--- app_admin/Controllers/C_Sess.php
+++ app_admin/Controllers/C_Sess.php
@@ -5,6 +5,6 @@ class C_Sess extends Controller
    function logout()
    {
       session_destroy();
-      header("Location: " . PC::BASE_URL . "Login");
+      header("Location: " . PC::BASE_URL_ADMIN . "Login");
    }
 }
