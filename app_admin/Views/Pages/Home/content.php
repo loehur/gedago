d031631f5dd91206a8fbf7f2c0616136e2b4929b
loehur <loehur@gmail.com>
2024-05-15 16:55:51 +0700

    data investor22

--- app_admin/Views/Pages/Home/content.php
+++ app_admin/Views/Pages/Home/content.php
@@ -12,36 +12,34 @@
         </section>
     </div>
 <?php } else { ?>
-    <div class="container-fluid border-0">
-        <div class="container">
-            <section>
-                <div class="row">
-                    <div class="col m-1 border rounded bg-white p-3" style="min-width: 300px;">
-                        <div class="row">
-                            <div class="col">
-                                <small>Admin Logged in</small><br>
-                                <span class="text-primary"><?= $_SESSION['log_admin']['nama'] ?></span>
-                            </div>
+    <div class="container">
+        <section>
+            <div class="row">
+                <div class="col m-1 border rounded bg-white p-3" style="min-width: 300px;">
+                    <div class="row">
+                        <div class="col">
+                            <small>Admin Logged in</small><br>
+                            <span class="text-primary"><?= $_SESSION['log_admin']['nama'] ?></span>
                         </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col">
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col">
 
-                                <small>Privileges</small><br>
+                            <small>Privileges</small><br>
 
-                                <span class="text-danger">
-                                    <?php foreach ($_SESSION['config']['access'] as $key => $acc) {
-                                        if ($acc['code'] == $_SESSION['log_admin']['access']) {
-                                            echo $acc['name'] . "";
-                                        }
-                                    } ?>
-                                </span>
-                            </div>
+                            <span class="text-danger">
+                                <?php foreach ($_SESSION['config']['access'] as $key => $acc) {
+                                    if ($acc['code'] == $_SESSION['log_admin']['access']) {
+                                        echo $acc['name'] . "";
+                                    }
+                                } ?>
+                            </span>
                         </div>
                     </div>
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
     </div>
 <?php } ?>
 
