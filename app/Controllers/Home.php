fab54637a64784f8d85361870f14195ad9530224
loehur <loehur@gmail.com>
2024-05-19 13:38:56 +0700

    wd auto22424w222323457545222

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -62,6 +62,20 @@ class Home extends Controller
          ],
       ];
 
+      if (isset($_SESSION['log'])) {
+         $d = $this->db(0)->get_where_row("portfolio", "user_id = '" . $_SESSION['log']['user_id'] . "' AND port_status = 0");
+         $data['port_balance'] = $this->func("Portfolio")->portfolio($d);
+         if (isset($data['port_balance']['data']['user_id'])) {
+            $data['checkin'] = $this->func("Portfolio")->daily_checkin_today($data['port_balance']);
+            $data['watch'] = $this->func("Portfolio")->daily_watch_today($data['port_balance']);
+         } else {
+            $data['checkin'] = [];
+            $data['watch'] = [];
+            $data['fee_dc'] = [];
+            $data['port_balance'] = [];
+         }
+      }
+
       $this->view(__CLASS__, __CLASS__ . "/content", $data);
    }
 }
